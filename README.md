8d67b4320fcf9daacf2a64ec022f161a985804ea
Jonathan Hope <jonathan.douglas.hope@gmail.com>
2019-07-08 21:16:20 -0700

    README and a license.

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Table of Contents
+
+1.  [mainspring-mode-line](#orgfd4edd5)
+    1.  [Overview](#org7d91936)
+    2.  [Installation](#org0a2e728)
+
+
+<a id="orgfd4edd5"></a>
+
+# mainspring-mode-line
+
+A pretty mode-line for Emacs.
+
+![img](mainspring-mode-line.png)
+
+
+<a id="org7d91936"></a>
+
+## Overview
+
+mainspring-mode-line is a mode-line for Emacs. It shows the number of the window, whether the buffer is dirty, the name of the buffer, the project if any, the current major mode, a scrollbar, and the coordinates of the cursor.
+
+
+<a id="org0a2e728"></a>
+
+## Installation
+
+This uses PragmataPro fonts which are non-free. It also relies on projectiel and winum.
--- mainspring-mode-line.el
+++ mainspring-mode-line.el
@@ -1,11 +1,26 @@
 ;;; mainspring-mode-line.el --- Custom mode line.
 
-;; Copyright (C) 2018 Jonathan Hope
+;; Copyright (C) 2019 Jonathan Hope
 
 ;; Author: Jonathan Hope <jonathan.douglas.hope@gmail.com>
 ;; Version: 1.0
 ;; Package-Requires ((projectile "0.15.0-cvs") (window-numbering "1.1.2"))
 ;; Keywords: mode-line
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
